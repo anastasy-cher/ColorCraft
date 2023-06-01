b946ac7e6e03d780fc9d6422fedbd40f47c115f4
anstcher <anastasiacher73@gmail.com>
2023-06-02 00:09:03 +0200

    Nodemailer configurado

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 // Se encarga de crear las cookies con passport
 const passport = require('passport')
 const strategy = require('passport-local').Strategy
+const nodemailer = require('nodemailer')
 
 // Traemos la bd para hacer consultas
 const pool = require('../config/conexion.js')
@@ -29,6 +30,79 @@ passport.use('local.registro', new strategy({
     }
     const [resultado] = await pool.query('INSERT INTO USERS set ?', [user])
     
+    // Enviamos el email de bienvenida
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: "chernykhanastasia96@gmail.com",
+            pass: process.env.EMAIL,
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: req.body.email + ' <chernykhanastasia96@gmail.com>',
+        to: req.body.email,
+        subject:` ¡Bienvenido ${name}!`,
+        html: `<!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Bienvenido a nuestro generador de paletas de colores</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f1f1f1;
+                    margin: 0;
+                    padding: 0;
+                }
+        
+                .container {
+                    max-width: 600px;
+                    margin: 0 auto;
+                    padding: 20px;
+                    background-color: #ffffff;
+                    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+                }
+        
+                h1 {
+                    color: #333333;
+                }
+        
+                p {
+                    color: #666666;
+                    margin-bottom: 20px;
+                }
+        
+                .cta-button {
+                    text-decoration: none;
+                    display: inline-block;
+                    margin-top: 30px;
+                    padding: 12px 24px;
+                    background-color: #ffffff;
+                    color: #ffffff;
+                    text-decoration: none;
+                    border-radius: 4px;
+                    border: 1px solid blue;
+                }
+        
+                
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <h1>Bienvenido ${name} a nuestro generador de paletas de colores</h1>
+                <p>¡Gracias por unirte a nuestra comunidad! Estamos emocionados de tenerte a bordo.</p>
+                <p>Con nuestro generador de paletas de colores, podrás explorar y crear combinaciones de colores sorprendentes para tus proyectos de diseño.</p>
+                <p>¡Comienza a crear tus paletas de colores hoy mismo y lleva tus diseños al siguiente nivel!</p>
+                <a href="https://colorcraft.onrender.com" class="cta-button">Crear paletas de colores</a>
+            </div>
+        </body>
+        </html>`
+      });
+
+    console.log("Message sent: ", info.messageId);
     // crear la cookie del usuario
     // Sacamos el id de la bd y se la añadimos al objeto de usuario
     user.id = resultado.insertId
